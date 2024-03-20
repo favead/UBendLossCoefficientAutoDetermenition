dd4ba838f12974eecbb2ef59b4270ae943c59b65
favead <diablo3_monk@mail.ru>
2024-03-20 16:01:27 +0300

    Add neural network training config

--- src/ubend_pl/configs.py
+++ src/ubend_pl/configs.py
@@ -4,11 +4,14 @@ from dataclasses import dataclass
 @dataclass
 class DataConfig:
     raw_data_path: str
+    normalized_data_path: str
     train_data_path: str
     val_data_path: str
     val_size: float
+    params_ranges: dict[str, list[float, float]]
 
 
 @dataclass
-class FeaturesConfig:
-    pass
+class NnTrainConfig:
+    artifact_dir: str
+    model_dir: str
