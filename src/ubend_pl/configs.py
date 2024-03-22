e0f49eee509c2aa101b2766096e5583aae02b6d7
favead <diablo3_monk@mail.ru>
2024-03-23 02:08:39 +0300

    Add configs for uncertainity active learning

--- src/ubend_pl/configs.py
+++ src/ubend_pl/configs.py
@@ -15,3 +15,29 @@ class DataConfig:
 class NnTrainConfig:
     artifact_dir: str
     model_dir: str
+
+
+@dataclass
+class GPrUncertainityConfig:
+    n_features: int
+    c_min: float
+    c_max: float
+    nu: float
+    ck_const: float
+    ck_min: float
+    ck_max: float
+    whtk_constant: float
+    whtk_min: float
+    whtk_max: float
+    n_restarts_optimizer: int
+
+
+@dataclass
+class GPrUncertainityTrainConfig:
+    artifact_dir: str
+    model_dir: str
+    train_data_path: str
+    val_data_path: str
+    n_start_points: int
+    n_query: int
+    estimation_step: int
