21bb155b854fef4d66c77503bac2a27aa6d57cb9
favead <diablo3_monk@mail.ru>
2024-03-24 18:38:40 +0300

    Add blank and notebook for visualization

---
+++ src/ubend_pl/visualization/visualize.py
@@ -0,0 +1,21 @@
+import glob
+from pathlib import Path
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+from ubend_pl.configs import VisualizationConfig
+
+
+@hydra.main(
+    version_base=None, config_name="config", config_path="../../../configs"
+)
+def visualize(cfg: DictConfig) -> None:
+    OmegaConf.to_yaml(cfg)
+    visualization_config = VisualizationConfig(cfg.get("visualization"))
+
+    for artifact_folder in glob.glob(visualization_config.artifact_dir):
+        
+
+
+if __name__ == "__main__":
+    visualize()
